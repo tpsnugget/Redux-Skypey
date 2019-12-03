135cebc9098ecb81a3e73150aad21f18d6a1d7c6
Michael Giebner <mikegiebner@gmail.com>
2019-12-03 12:48:26 -0500

    Refactored into containers and components folders

--- skypey/src/containers/App.js
+++ skypey/src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Sidebar from "./Sidebar"
-import Main from "./Main"
-import store from "./store"
+import Sidebar from "../components/Sidebar"
+import Main from "../components/Main"
+import store from "../store"
 import _ from "lodash"
 
 import './App.css';
